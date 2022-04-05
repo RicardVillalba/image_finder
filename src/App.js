0d17f09218d8097b21e2b68ec7850a4bd719630b
RicardVillalba <ricardsax@gmail.com>
2022-04-05 14:22:02 +0200

    rendering images on screen

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Formik, Form, Field } from "formik";
 import "./components/Header.css";
 const App = () => {
   const [images, setImages] = useState([]);
+  const open = (url) => window.open(url);
   console.log({ images });
   return (
     <div>
@@ -27,6 +28,21 @@ const App = () => {
           </Form>
         </Formik>
       </header>
+      <div className="container">
+        <div className="center">
+          {images.map((image) => (
+            <article
+              key={image.id}
+              onClick={() => {
+                open(image.links.html);
+              }}
+            >
+              <img src={image.urls.regular} alt="foto" />
+              <p>{[image.description, image.alt_description].join("-")}</p>
+            </article>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
