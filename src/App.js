48e094e1bf8f667d8bd458f57f1c74e61bd3bde4
RicardVillalba <ricardsax@gmail.com>
2022-04-05 12:56:41 +0200

    search form created styles added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Formik, Form, Field } from "formik";
+import "./components/Header.css";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: "" }}
+          onSubmit={async (values) => {
+            //API call
+            console.log(values);
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search"></Field>
+          </Form>
+        </Formik>
       </header>
     </div>
   );
-}
+};
 
 export default App;
