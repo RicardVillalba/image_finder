6f25e283e576345f20044b9081d8f6fd577c9c82
RicardVillalba <ricardsax@gmail.com>
2022-04-05 13:48:59 +0200

    api connected

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ const App = () => {
         <Formik
           initialValues={{ search: "" }}
           onSubmit={async (values) => {
-            //API call
-            console.log(values);
+            const response = await fetch(
+              `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+              {
+                headers: {
+                  Authorization: `Client-ID ${process.env.REACT_APP_API_KEY}`,
+                },
+              }
+            );
+            const data = await response.json();
+            console.log(data);
           }}
         >
           <Form>
